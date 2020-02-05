05bc83320e40d11a2847eb5ec3785f8876caad47
cuddjim <57361868+cuddjim@users.noreply.github.com>
2020-02-05 15:31:35 -0500

    shell

---
+++ app.R
@@ -0,0 +1,121 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  uiOutput('page_content')
+  
+)
+
+translator <- Translator$new(translation_json_path = "translation")
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  
+  # select and translate language
+  tr <- reactive({
+    
+    selected <- 'fr'#input$selected_language
+    
+    if (length(selected) > 0 && selected == TRUE) {
+      translator$set_translation_language('en')
+    }
+    
+    else {
+      
+      translator$set_translation_language('fr')
+      
+    }
+    
+    translator
+    
+  })
+  
+  output$page_content <- renderUI({
+    
+    navbarPage('',
+               
+               tabPanel(
+                 
+                 tr()$t('Crop Production'),
+                 
+                 tabsetPanel(
+                   
+                   type='tabs',
+                   
+                   tabPanel(
+                     
+                     'Map',
+                     
+                     sidebarPanel(
+                       selectizeInput("map_commodity", label = 'Select Fuel Type',
+                                      choices = c('a','b'),
+                                      selected = 'a'),
+                       sliderInput("year", label = 'Select Year:',2005, 2018, value=2018,
+                                   step=1,
+                                   animate=TRUE,
+                                   sep = ""),
+                       width=2
+                     ),
+                     
+                     mainPanel(
+                       leafletOutput("plot",height=600),
+                       width=10
+                     )
+                     
+                   ),
+                   
+                   
+                   tabPanel(
+                     
+                     'Bubble chart',
+                     
+                     sidebarPanel(
+                       selectizeInput("province", label = 'Select Province',
+                                      choices = c('a','b'), selected='a'),
+                       width=3
+                     ),
+                     
+                     mainPanel(
+                       fluidRow(
+                         plotlyOutput("bubble", height = '650px')
+                       ),
+                       width=8
+                     )
+                     
+                   ),
+                   
+                   tabPanel(
+                     
+                     'Line chart',
+                     
+                     mainPanel(
+                       fluidRow(
+                         plotlyOutput("line", height = '550px')
+                       ),
+                       width=8
+                     )
+                     
+                   ),
+                   tabPanel(
+                     
+                     'Data stories',
+                     
+                     mainPanel(
+                       dataTableOutput("storytable"),
+                       width=12
+                     )
+                     
+                   )
+                   
+                 )
+               )
+    )
+    
+  })
+  
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
