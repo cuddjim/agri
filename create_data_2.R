915b90d2df63445fe88cee98b3bb39d081576237
cuddjim <57361868+cuddjim@users.noreply.github.com>
2020-02-10 15:48:09 -0500

    conveerting sads

--- create_data_2.R
+++ create_data_2.R
@@ -1,6 +1,6 @@
 
 ## load libraries
-library(cansim); library(tidyverse); library(janitor)
+library(cansim); library(tidyverse); library(janitor); library(rgeos); library(rgdal)
 
 ## create dictionary
 names_of_sets = c('grain_area', 'grain_prod', 'can_snd','corn_snd', 'farm_snd')
@@ -11,12 +11,23 @@ sad_conversion = read_csv('sad_conversion.csv')
 list_of_sets = map(location_of_sets, function(x) get_cansim_ndm(x) %>% clean_names())
 names(list_of_sets) = names_of_sets
 
+sad_map = readOGR('CropsSADRegions_2017_Gen/CropsSADRegions_2017_Gen.shp',stringsAsFactors = FALSE)
+
+
+list_of_sets[['grain_area']] %>% count(geo)
+
+convert_sad = read.csv('convert_sad.csv', encoding='Latin//ASCII') %>% 
+  mutate(geo=iconv(GEO,to='ASCII//TRANSLIT')) %>%
+  select(geo,SADRegion)
+
+
 ## convert new sasksatchewan regions
 list_of_sets[['grain_area']] = rbind(
   
   # create set containing all observations which do not need modification
   list_of_sets[['grain_area']] %>% select(ref_date,geo,type_of_crop,harvest_disposition,value) %>%
     filter(!(grepl('Small Area Data',geo) & grepl('Saskatchewan',geo) & ref_date %in% 2014:2016)) %>%
+    mutate(geo=iconv(geo,from='UTF-8',to='ASCII//TRANSLIT')) %>%
     data.frame() ,
   
   # create set containg all observations which need modification
@@ -30,7 +41,10 @@ list_of_sets[['grain_area']] = rbind(
         enframe(name='geo', value='value')
       } ) %>%
     # modify geo names
-    mutate(geo=paste0('Small Area Data ',geo)) %>%
-    data.frame()
+    mutate(geo=paste0('Small Area Data ',geo),
+           geo=iconv(geo,from='UTF-8',to='ASCII//TRANSLIT')) %>%
+    data.frame() ) %>%
+  left_join(convert_sad, by='geo')
 
-)
+list_of_sets[['grain_area']] %>% count(SADRegion, sort=TRUE)
+no = list_of_sets[['grain_area']]
