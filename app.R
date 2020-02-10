e4331b85b11a9d60a2082058f456cf23f3a400bd
cuddjim <57361868+cuddjim@users.noreply.github.com>
2020-02-10 16:17:42 -0500

    skeleton

--- app.R
+++ app.R
@@ -3,7 +3,8 @@ library(shiny)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
-  uiOutput('page_content')
+  uiOutput('page_content')#,
+  # uiOutput('page_content_1')
   
 )
 
@@ -34,7 +35,10 @@ server <- function(input, output) {
   
   output$page_content <- renderUI({
     
-    navbarPage('',
+    navbarPage(title = div(span(img(src = "wheat2.png"),
+                                "Crops and grains in Canada",
+                                style = "position: relative; top: 80%; transform: translateY(10%);")),
+               
                
                tabPanel(
                  
@@ -110,10 +114,140 @@ server <- function(input, output) {
                    )
                    
                  )
+               ),
+               tabPanel(
+                 
+                 tr()$t('Farm supply and disposition of grains'),
+                 
+                 tabsetPanel(
+                   
+                   type='tabs',
+                   
+                   tabPanel(
+                     
+                     'Map',
+                     
+                     sidebarPanel(
+                       selectizeInput("map_commodity", label = 'Select Fuel Type',
+                                      choices = c('a','b'),
+                                      selected = 'a'),
+                       sliderInput("year", label = 'Select Year:',2005, 2018, value=2018,
+                                   step=1,
+                                   animate=TRUE,
+                                   sep = ""),
+                       width=2
+                     ),
+                     
+                     mainPanel(
+                       leafletOutput("plot",height=600),
+                       width=10
+                     )
+                     
+                   ),
+                   
+                   
+                   tabPanel(
+                     
+                     'Bubble chart',
+                     
+                     sidebarPanel(
+                       selectizeInput("province", label = 'Select Province',
+                                      choices = c('a','b'), selected='a'),
+                       width=3
+                     ),
+                     
+                     mainPanel(
+                       fluidRow(
+                         plotlyOutput("bubble", height = '650px')
+                       ),
+                       width=8
+                     )
+                     
+                   ),
+                   
+                   tabPanel(
+                     
+                     'Line chart',
+                     
+                     mainPanel(
+                       fluidRow(
+                         plotlyOutput("line", height = '550px')
+                       ),
+                       width=8
+                     )
+                     
+                   ),
+                   tabPanel(
+                     
+                     'Data stories',
+                     
+                     mainPanel(
+                       dataTableOutput("storytable"),
+                       width=12
+                     )
+                     
+                   )
+                   
+                 )
+               ),
+               tabPanel(
+                 
+                 tr()$t('Supply and disposition of grains in Canada'),
+                 
+                 tabsetPanel(
+                   
+                   type='tabs',
+                   
+                   
+                   tabPanel(
+                     
+                     'Bubble chart',
+                     
+                     sidebarPanel(
+                       selectizeInput("province", label = 'Select Province',
+                                      choices = c('a','b'), selected='a'),
+                       width=3
+                     ),
+                     
+                     mainPanel(
+                       fluidRow(
+                         plotlyOutput("bubble", height = '650px')
+                       ),
+                       width=8
+                     )
+                     
+                   ),
+                   
+                   tabPanel(
+                     
+                     'Line chart',
+                     
+                     mainPanel(
+                       fluidRow(
+                         plotlyOutput("line", height = '550px')
+                       ),
+                       width=8
+                     )
+                     
+                   ),
+                   tabPanel(
+                     
+                     'Data stories',
+                     
+                     mainPanel(
+                       dataTableOutput("storytable"),
+                       width=12
+                     )
+                     
+                   )
+                   
+                 )
                )
+               
     )
     
   })
+  
   
 }
 
