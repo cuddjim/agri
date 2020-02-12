1d9da42b24ae0945dc790288e7767e6e612aaa9f
cuddjim <57361868+cuddjim@users.noreply.github.com>
2020-02-12 15:40:31 -0500

    add language

--- app.R
+++ app.R
@@ -1,10 +1,17 @@
+
 library(shiny); library(shiny.i18n)
 library(leaflet); library(plotly)
-library(DT)
+library(DT); library(shinyWidgets)
+
+sad_map = readOGR('CropsSADRegions_2017_Gen/CropsSADRegions_2017_Gen.shp',stringsAsFactors = FALSE)
+sad_map@data$PRSADReg=as.numeric(sad_map@data$PRSADReg)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
+  
+  tags$script("$(\"input:radio[name='selected_language'][value='fr']\").parent().css('background-color', '#FFFFFF');"),
+  uiOutput('select_language'),
   uiOutput('page_content')
   
 )
@@ -18,7 +25,7 @@ server <- function(input, output) {
   # select and translate language
   tr <- reactive({
     
-    selected <- 'en'#input$selected_language
+    selected <- input$selected_language
     
     if (length(selected) > 0 && selected == TRUE) {
       translator$set_translation_language('en')
@@ -34,15 +41,54 @@ server <- function(input, output) {
     
   })
   
-  farm_snd_table <- reactive({
+  map_reactive <- reactive({
     
+    # min_year = min(input$year); max_year = max(input$year)
+    # map_commodity = input$map_commodity
+    # min_emissions = str_c(map_commodity,'_emission_',min_year); max_emissions = str_c(map_commodity,'_emission_',max_year)
+    # min_outputs = str_c(map_commodity,'_output_',min_year); max_outputs = str_c(map_commodity,'_output_',max_year)
+    # 
+    # prov_map@data %<>% 
+    #   mutate(emissions=round(rowMeans(select(.,min_emissions:max_emissions),na.rm=TRUE),0),
+    #          outputs=round(rowMeans(select(.,min_outputs:max_outputs),na.rm=TRUE),0)) %>% 
+    #   mutate(scaled_outputs = log(1+outputs)^2.5) %>%
+    #   mutate(mean_output = mean(scaled_outputs,na.rm=TRUE)) %>%
+    #   mutate(outputs_1=23*scaled_outputs/mean_output)
+    
+    sad_map
+    
+  })
+  
+  output$plot <- renderLeaflet({
+    
+    leaflet(options = leafletOptions(minZoom = 4, maxZoom = 2,
+                                     attributionControl=FALSE)) %>%
+      setView(lng = -98.4, lat = 58.2, zoom = 4) %>%
+      addProviderTiles(providers$Wikimedia,
+                       options = providerTileOptions(opacity = 0.8)) %>%
+      clearShapes() %>%
+      clearControls() %>%
+      addPolygons(data = map_reactive(),
+                  fillColor = ~colorBin(c("#E1F5C4","#EDE574","#F9D423","#FC913A","#FF4E50"), PRSADReg, 5)(PRSADReg),
+                  color = "#BDBDC3",
+                  fillOpacity = 0.7,
+                  weight = 4)
+    
+  })
+  
+  farm_snd_table <- reactive({
     
     selected_geo = input$province_farm
     selected_crop = input$crop_farm
+    selected_variable = input$var_farm
     
     farm_snd %>% spread(ref_date, value) %>% 
       select(c('geo','type_of_crop','farm_supply_and_disposition_of_grains',ends_with('07'),ends_with('03'),ends_with('12'))) %>% 
-      filter(geo == selected_geo, type_of_crop == selected_crop)
+      filter(
+        geo == selected_geo, 
+        type_of_crop == selected_crop,
+        farm_supply_and_disposition_of_grains == selected_variable) %>%
+      rename(Province=geo,Crop=type_of_crop,Variable=farm_supply_and_disposition_of_grains)
     
   })
 
@@ -57,7 +103,7 @@ server <- function(input, output) {
                              pageLength = 24,
                              initComplete = JS(
                                "function(settings, json) {",
-                               "$(this.api().table().header()).css({'background-color': '#03085B', 'color': '#fff'});",
+                               "$(this.api().table().header()).css({'font-size': '12px', 'background-color': '#03085B', 'color': '#fff'});",
                                "}")))
     
   })
@@ -83,6 +129,33 @@ server <- function(input, output) {
       add_trace(data = farm_snd_graph(), type = 'scatter',mode = 'markers') %>% 
       add_trace(data = farm_snd_graph(),type = 'scatter',mode = 'markers')
 
+  })
+  
+  output$select_language <- renderUI({
+    
+    fluidPage(
+      
+      tabPanel(
+        
+        'Language',
+        
+        column(switchInput(
+          inputId = "selected_language",
+          value = TRUE,
+          onLabel = 'FR',
+          offLabel = 'EN',
+          onStatus = '#000000',
+          offStatus = '#FFFFFF',
+          labelWidth='auto',
+          handleWidth='auto',
+          size='normal',
+          width='100px'
+        ), width=4,offset=10),
+        icon = NULL
+        
+      )
+    )
+    
   })
   
   output$page_content <- renderUI({
@@ -155,6 +228,8 @@ server <- function(input, output) {
                                       choices = unique(farm_snd$geo), selected='Canada'),
                        selectizeInput("crop_farm", label = 'Select Crop',
                                       choices = unique(farm_snd$type_of_crop), selected='All wheat'),
+                       selectizeInput("var_farm", label = 'Select Variable',
+                                      choices = unique(farm_snd$farm_supply_and_disposition_of_grains), selected='Production'),
                        width=3
                      ),
                      
