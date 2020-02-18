181957befaad163597bdddfd3f7c90fd46a5675d
spazznolo <jeremie.spagnolo@gmail.com>
2020-02-17 20:42:35 -0500

    change create data

--- create_data.R
+++ create_data.R
@@ -1,58 +1,67 @@
-library(cansim);library(tidyverse);library(janitor)
 
-pids <- c('32100002','32100013','32100014','32100015','32100359')
+## load libraries
+library(cansim); library(tidyverse); library(janitor); library(rgeos); library(rgdal)
+library(stringi); library(magrittr)
 
-sad_conversion <- read.csv("C:/Users/Jimmy/OneDrive/Documents/GitHub/agri/sad_conversion.csv")
+## create dictionary
+names_of_sets = c('grain_area', 'can_snd', 'farm_snd')
+location_of_sets = c('32100002', '32100013', '32100015')
 
-for (pid in pids) {
-  temp = get_cansim_ndm(paste0(pid)) %>% clean_names()
-  assign(paste0('T',str_sub(pid,6,8)),temp)
-}
-
-# create saskatchewan set
-sk_sad = T002 %>% 
-  filter(grepl('Small Area Data',geo) & grepl('Saskatchewan',geo) & ref_date %in% 1976:2016) %>%
-  add_count(harvest_disposition, type_of_crop) %>%
-  filter(n>51) %>% select(-n) %>%
-  data.frame()
-
-# create shell for input
-sk_shell = data.frame(matrix(NA,0,0))
-
-# convert old SADs to new with sad_conversion table
-for (crop in unique(sk_sad$type_of_crop)) {
-  
-  for (harvest in unique(sk_sad$harvest_disposition)) {
-    
-    for (year in 1976:2016) {
-      
-      temp = sk_sad %>% filter(type_of_crop == crop & harvest_disposition == harvest & ref_date == year) %>% data.frame()
-      
-      if (nrow(temp)==37) {
-        
-        temp[21:37,'value'] = unname(colSums(sad_conversion*temp[1:20,'value'],na.rm=TRUE))
-        temp %<>% filter(grepl('[(]',geo)) %>% data.frame()
-        sk_shell = rbind(sk_shell,temp)
-        
-      }
-    }
-  }
-}
-
-
-# create new set
-T002 %<>% filter(!(grepl('Small Area Data',geo) & grepl('Saskatchewan',geo) & ref_date %in% 1976:2016)) %>% rbind(.,sk_shell) %>% 
-  filter(harvest_disposition %in% c("Seeded area (acres)", "harvested area (acres)", "Average yield (bushels per acre)", "Production (metric tonnes)") 
-         & type_of_crop %in% c("Barley", "Oats", "Soybeans", "Canola", "Wheat, all")) %>%
-  mutate(geo=iconv(geo,from='UTF-8',to='ASCII//TRANSLIT'))
-
+sad_conversion = read_csv('sad_conversion.csv')
 sad_name_to_code = read_csv('sad_name_to_code.csv')
-sad_name_to_code$sad_name = sub("\\s*\\(.*", "", sad_name_to_code$sad_name)
-sad_name_to_code %<>%
-  add_row(sad_name='Small Area Data Region 18 - Saskatchewan',sad_code='4718') %>%
-  add_row(sad_name='Small Area Data Region 19 - Saskatchewan',sad_code='4719') %>%
-  add_row(sad_name='Small Area Data Region 20 - Saskatchewan',sad_code='4720')
-  
 
-T002 %<>% mutate(geo=sub("\\s*\\(.*", "", geo)) %>% 
-  left_join(sad_name_to_code,by=c('geo'='sad_name'))
+## create list of ndm sets
+list_of_sets = map(location_of_sets, 
+                   function(x) get_cansim_ndm(x) %>% 
+                     clean_names %>%
+                     mutate(geo=stri_trans_general(geo,"Latin-ASCII")) )
+
+names(list_of_sets) = names_of_sets
+
+
+## convert new sasksatchewan regions
+rbind(
+  
+  # create set containing all observations which do not need modification
+  list_of_sets[['grain_area']] %>% select(ref_date,geo,type_of_crop,harvest_disposition,value) %>%
+    filter(!(grepl('Small Area Data',geo) & grepl('Saskatchewan',geo) & ref_date %in% 1976:2016)) %>%
+    #mutate(geo=iconv(geo,from='UTF-8',to='ASCII//TRANSLIT')) %>%
+    data.frame() ,
+  
+  # create set containg all observations which need modification
+  list_of_sets[['grain_area']] %>% select(ref_date,geo,type_of_crop,harvest_disposition,value) %>%
+    filter(grepl('Small Area Data',geo) & grepl('Saskatchewan',geo) & ref_date %in% 1976:2016) %>%
+    # group by crop, year and harvest disposition
+    group_by(type_of_crop,ref_date,harvest_disposition) %>%
+    # convert SAD regions
+    group_modify(~ {
+      colSums(.$value*sad_conversion,na.rm=TRUE) %>%
+        enframe(name='geo', value='value')
+      } ) %>%
+    # modify geo names
+    mutate(geo=paste0('Small Area Data ',geo)
+           #,geo=iconv(geo,from='UTF-8',to='ASCII//TRANSLIT')
+           ) %>%
+    data.frame() ) %>%
+  left_join(sad_name_to_code, by=c('geo'='sad_name')) %>%
+  mutate(sad_code = ifelse(is.na(sad_code),parse_number(geo),sad_code)) %>%
+  filter(
+    harvest_disposition %in% c("Seeded area (acres)", "Harvested area (acres)", "Average yield (bushels per acre)", "Production (metric tonnes)") &
+    type_of_crop %in% c('Barley','Oats','Wheat, all','Canola','Peas, dry','Rye, fall remaining','Lentils','Flaxseed','Mustard seed')
+    ) %>%
+  mutate(
+    type_of_crop=gsub("(.*),.*", "\\1", tolower(type_of_crop)),
+    harvest_disposition = str_replace(harvest_disposition, " \\(.*\\)", ""),
+    harvest_disposition = gsub(" ","_",tolower(harvest_disposition))
+    )  -> list_of_sets[['grain_area']]
+
+
+## create sad map
+sad_map = readOGR('CropsSADRegions_2017_Gen/CropsSADRegions_2017_Gen.shp',stringsAsFactors = FALSE)
+sad_map@data %<>% left_join(list_of_sets[['grain_area']] %>%
+                              unite(variable,c('type_of_crop','harvest_disposition','ref_date')) %>%
+                              select(variable,value,sad_code,geo) %>%
+                              spread(variable,value) %>%
+                              mutate(sad_code = as.character(sad_code)), by=c('PRSADReg'='sad_code'))
+
+
